a10c49cbf89200d42f6f242e3a2412088ccb617c
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2022-05-30 16:56:43 -0300

    requisito 13

--- desafio12.sql
+++ desafio12.sql
@@ -1,2 +1,2 @@
-SELECT submitted_dates FROM northwind.purchase_orders
-WHERE submitted_dates LIKE '2006-04-26%';
+SELECT submitted_date FROM northwind.purchase_orders
+WHERE submitted_date LIKE '2006-04-26%';
